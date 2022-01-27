030acb3e387789add9b1ab7069cad4f578e0a8e8
CUI Hao <cuihao.leo@gmail.com>
2022-01-26 16:15:07 -0800

    add html preprocessor

---
+++ html_preprocessor/crawler.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""Download a web page and export the accessibility tree for parsing."""
+
+import argparse
+import json
+from pathlib import Path
+
+from playwright.sync_api import sync_playwright
+from requests_cache import CachedSession
+
+READABILITY_JS_URL = "https://raw.githubusercontent.com/mozilla/readability/5ea9c2f/Readability.js"
+
+
+def get_readability_js():
+    session = CachedSession("py_request_cache", backend="filesystem", use_temp=True)
+    res = session.get(READABILITY_JS_URL)
+    res.raise_for_status()
+    return res.text
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="input URL")
+    parser.add_argument("output", help="output dir")
+    args = parser.parse_args()
+
+    with sync_playwright() as p:
+        # firefox generates simpler accessibility tree than chromium
+        browser = p.firefox.launch()
+
+        page = browser.new_page()
+        page.set_viewport_size({"width": 1080, "height": 1920})
+        page.goto(args.url)
+
+        # apply readability.js
+        page.add_script_tag(content=get_readability_js())
+        cleaned_html = page.evaluate("""() => {
+            const documentClone = document.cloneNode(true); 
+            const article = new Readability(document).parse();
+            return article.content;
+        }""", [])
+        page.set_content(cleaned_html)
+
+        # obtain the accessibility tree
+        snapshot = page.accessibility.snapshot(interesting_only=False)
+
+        output_dir = Path(args.output)
+        output_dir.mkdir(exist_ok=True)
+
+        with open(output_dir / "cleaned.html", "w") as fout:
+            fout.write(cleaned_html)
+
+        with open(output_dir / "accessibility_tree.json", "w") as fout:
+            json.dump(snapshot, fout)
+
+        print(page.title())
+        browser.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ html_preprocessor/process_document.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+"""Process accessibility tree into DocumentSegment objects"""
+
+import argparse
+import json
+from pathlib import Path
+
+
+class DocumentSegment:
+    def __init__(self, segment_type, link, text):
+        self.type = segment_type
+        self.link = link
+        self.text = text
+
+    def __repr__(self):
+        if self.type == "listitem":
+            return f"({self.type}) FOLLOWING {repr(self.link)}"
+        else:
+            return f"({self.type}) {repr(self.text)}"
+
+
+def process_accessibility_tree(tree):
+    heading_stack = [(-1, None)]
+    segments = []
+
+    def extract_text(node):
+        if node["role"] in ["img", "list item marker"]:
+            return
+
+        if "children" in node:
+            for child in node.get("children", []):
+                yield from extract_text(child)
+        else:
+            yield node["name"].strip()
+
+    def iterate(node):
+        if node["role"] == "table":
+            # TODO: parse tables
+            return
+
+        if node["role"] in ["document", "landmark", "section"]:
+            for child in node.get("children", []):
+                iterate(child)
+        elif node["role"] == "heading":
+            text = " ".join(extract_text(node))
+            level = node["level"]
+
+            while heading_stack[-1][0] >= level:
+                heading_stack.pop()
+
+            link = heading_stack[-1][1]
+            heading_segment = DocumentSegment("heading", link, text)
+            heading_stack.append((level, heading_segment))
+            segments.append(heading_segment)
+        elif node["role"] == "list":
+            link = segments[-1]
+
+            for child in node.get("children", []):
+                if child["role"] != "listitem":
+                    raise ValueError("Invalid child element of a list")
+
+                listitem_segment = DocumentSegment("listitem", link, "")
+                segments.append(listitem_segment)
+                text_buffer = []
+
+                for grandchild in child.get("children", []):
+                    if grandchild["role"] == "list":
+                        if len(text_buffer) > 0:
+                            seg_in_list = DocumentSegment("text", listitem_segment, " ".join(text_buffer))
+                            segments.append(seg_in_list)
+                            text_buffer.clear()
+
+                        iterate(grandchild)
+                    else:
+                        text_buffer.extend(extract_text(grandchild))
+
+                if len(text_buffer) > 0:
+                    seg_in_list = DocumentSegment("text", listitem_segment, " ".join(text_buffer))
+                    segments.append(seg_in_list)
+        elif node["role"] in ["paragraph", "link"]:
+            link = heading_stack[-1][1]
+
+            text = " ".join(extract_text(node))
+            if text:
+                segments.append(DocumentSegment("text", link, text))
+        else:
+            raise ValueError(f"Invalid role: {node['role']}")
+
+    iterate(tree)
+    return segments
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("workdir", help="working directory")
+    args = parser.parse_args()
+
+    workdir = Path(args.workdir)
+    with open(workdir / "accessibility_tree.json") as fin:
+        accessibility_tree = json.load(fin)
+
+    segments = process_accessibility_tree(accessibility_tree)
+    for seg in segments:
+        print(seg)
+
+
+if __name__ == "__main__":
+    main()
