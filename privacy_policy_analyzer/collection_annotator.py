6f9d5ed6c4aeb650931c95c4286f881a704d120a
CUI Hao <cuih7@uci.edu>
2022-04-13 10:48:02 -0700

    improve collection annotator

--- privacy_policy_analyzer/collection_annotator.py
+++ privacy_policy_analyzer/collection_annotator.py
@@ -4,27 +4,6 @@ from types import SimpleNamespace
 import yaml
 
 import privacy_policy_analyzer
-from privacy_policy_analyzer.utils import get_conjuncts
-
-
-def discover_entities(token):
-    def dfs(token, path):
-        if token.dep_ in ["conj", "appos"]:
-            for child in token.children:
-                yield from dfs(child, path)
-        elif token._.ent is None:
-            for child in token.children:
-                yield from dfs(child, (token,) + path)
-        else:
-            yield [token, *path]
-
-    yield from dfs(token, ())
-
-
-def get_first_head(token):
-    while token.dep_ in ["conj", "appos"]:
-        token = token.head
-    return token
 
 
 class Pattern:
@@ -66,10 +45,7 @@ class Pattern:
             if not test_lemma(token, specs.lemma):
                 return False
 
-            if specs.dep == "root_like":
-                if any(t.pos_ == "VERB" for t in p.ancestors):
-                    return False
-            elif specs.dep not in ["*", p.dep_.lower()]:
+            if specs.dep not in ["*", p.dep_.lower()]:
                 return False
 
             if specs.pos not in ["*", token.pos_]:
@@ -77,17 +53,36 @@ class Pattern:
 
             return True
 
-        if len(chain) != len(self.pattern):
-            return False
+        i = 0
+        for specs in self.pattern:
+            if i == len(chain):
+                return False
 
-        for conj_tokens, specs in zip(chain, self.pattern):
+            current_token = chain[i]
+            conj_tokens = [current_token]
+
+            if current_token.dep_ in ["conj", "appos"]:
+                while current_token.dep_ in ["conj", "appos"]:
+                    current_token = current_token.head
+                    conj_tokens.append(current_token)
+
+                    if current_token == chain[i + 1]:
+                        i += 1
+            else:
+                while i + 1 < len(chain) and chain[i + 1].dep_ in ["conj", "appos"]:
+                    i += 1
+                    current_token = chain[i]
+                    conj_tokens.append(current_token)
+
             for token in conj_tokens:
                 if test(token, specs):
                     break
             else:
                 return False
 
-        return True
+            i += 1
+
+        return i == len(chain)
 
 
 class CollectionAnnotator:
@@ -124,48 +119,60 @@ class CollectionAnnotator:
 
             return False
 
-        possible_datatypes = set()
+        dtype_candidates = []
+        actor_candidates = []
 
         for e in doc.ents:
             if like_data(e[0]):
-                possible_datatypes.add(get_first_head(e.root))
+                dtype_candidates.append((e, e.root, list(e.root.ancestors)[::-1]))
+            else:
+                actor_candidates.append((e, e.root, list(e.root.ancestors)[::-1]))
 
-        for datatype_token in possible_datatypes:
-            datatype_chain = [datatype_token]
-            current_token = datatype_token
+        for dtype, dtype_root, dtype_ancestors in dtype_candidates:
+            for actor, actor_root, actor_ancestors in actor_candidates:
+                if dtype_root.is_ancestor(actor_root) or actor_root.is_ancestor(dtype_root):
+                    continue
 
-            while current_token.dep_ != "ROOT" and not current_token.is_space:
-                parent_token = get_first_head(current_token.head)
-                datatype_chain.append(parent_token)
+                # Remove common ancestors
+                common_ancestor_count = 0
+                for a, b in zip(dtype_ancestors, actor_ancestors):
+                    if a == b:
+                        common_ancestor_count += 1
+                    else:
+                        break
 
-                if parent_token.pos_ == "VERB":
-                    for child in parent_token.children:
-                        if child == current_token:
-                            continue
+                if common_ancestor_count == 0:
+                    # Skip if not in the same sentence
+                    continue
 
-                        for entity_chain in discover_entities(child):
-                            full_chain = [[t, *get_conjuncts(t)] for t in entity_chain]
-                            full_chain.extend([t, *get_conjuncts(t)] for t in datatype_chain[::-1])
+                # Skip if actor and dtype are not connected by a verb
+                verb = dtype_ancestors[common_ancestor_count - 1]
+                if verb.pos_ != "VERB":
+                    continue
 
-                            if any(p(full_chain) for p in self.patterns):
-                                entity_tokens, *intermediate_chain, datatype_tokens = full_chain
+                full_chain = [actor_root]
+                full_chain.extend(actor_ancestors[-1:common_ancestor_count-1:-1])
+                full_chain.append(verb)
+                full_chain.extend(dtype_ancestors[common_ancestor_count::])
+                full_chain.append(dtype_root)
 
-                                entities = [t._.ent for t in entity_tokens]
-                                datatypes = [t._.ent for t in datatype_tokens]
+                if any(p(full_chain) for p in self.patterns):
+                    negation = False
 
-                                print("#" * 40)
-                                print(current_token.sent, end="\n\n")
-                                print(f"> DEP: {entity_tokens[0].dep_}, POS: {entity_tokens[0].pos_}, ENTITIES:", entities)
-                                for item in intermediate_chain:
-                                    print(f"> DEP: {item[0].dep_}, POS: {item[0].pos_}, TOKEN:", [t.text for t in item])
-                                print(f"> DEP: {datatype_tokens[0].dep_}, POS: {datatype_tokens[0].pos_}, DATATYPES:", datatypes)
-                                print("#" * 40)
+                    for token in full_chain:
+                        for child in token.children:
+                            if child.dep_ == "neg":
+                                negation = True
+                                break
 
-                                for et in entity_tokens:
-                                    if et.ent_type_ != "DATA":
-                                        for dt in datatype_tokens:
-                                            if dt.ent_type_ in ["DATA", "NN"]:
-                                                policy_document.link(et, dt, "COLLECT")
-                                                policy_document.link(dt, et, "COLLECTED_BY")
+                    print("#" * 40)
+                    print(verb.sent, end="\n\n")
 
-                current_token = parent_token
+                    if not negation:
+                        print(">", actor, verb.lemma_.upper(), dtype)
+                        policy_document.link(actor[0], dtype[0], "COLLECT")
+                        policy_document.link(dtype[0], actor[0], "COLLECTED_BY")
+                    else:
+                        print("> (NEG)", actor, verb.lemma_.upper(), dtype)
+
+                    print("#" * 40)
