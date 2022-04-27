b985dc438faf67f089af62756294e53a6bd12ff8
CUI Hao <cuihao.leo@gmail.com>
2022-04-27 09:53:58 -0700

    upload dataset preprocessing scripts

---
+++ dataset_preprocessing/policheck/fetch_wayback_machine.py
@@ -0,0 +1,115 @@
+import argparse
+import csv
+import datetime as dt
+import hashlib
+import logging
+import math
+import os
+import pickle
+import random
+import time
+
+import requests
+from requests.exceptions import HTTPError
+from werkzeug.urls import url_fix
+
+logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
+
+DATE_RANGE = ("20180101", "20191231")
+DATE_REF = "20190101"
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("flow_csv", help="PoliCheck flow.csv path")
+    parser.add_argument("out_dir", help="Output directory")
+    args = parser.parse_args()
+
+    all_urls = set()
+
+    # read PoliCheck's flow.csv
+    with open(args.flow_csv, newline="") as fin:
+        for row in csv.DictReader(fin, fieldnames=list("01234567") + ["policy_url"]):
+            all_urls.add(url_fix(row["policy_url"]))
+
+    # shuffle URLs so multiple servers crawl in different orders
+    all_urls = list(all_urls)
+    random.shuffle(all_urls)
+
+    # Wayback CDX API params
+    cdx_params = {
+        "url": "",
+        "fl": "timestamp",
+        "from": DATE_RANGE[0],
+        "to": DATE_RANGE[1],
+        "collapse": "timestamp:8",  # 1 result per day
+        "filter": ["statuscode:200", "mimetype:text/html"],
+    }
+
+    reference_date = dt.datetime.strptime(DATE_REF, "%Y%m%d")
+    os.makedirs(args.out_dir, exist_ok=True)
+    wait_seconds = 1
+
+    for url in all_urls:
+        # use hashed URL as filename
+        url_hash = hashlib.blake2s(url.encode()).hexdigest()
+        out_path = os.path.join(args.out_dir, f"{url_hash}.pkl")
+
+        if os.path.lexists(out_path):
+            continue
+
+        cdx_params["url"] = url
+        logging.info("Current URL: %r", url)
+        time.sleep(wait_seconds)
+
+        # Wayback CDX API
+        try:
+            req = requests.get("https://web.archive.org/cdx/search/cdx", params=cdx_params)
+            req.raise_for_status()
+        except HTTPError:
+            runtime_error = req.headers.get("x-archive-wayback-runtime-error", "")
+            if not runtime_error.endswith("Blocked Site Error"):
+                wait_seconds *= 2
+            continue
+
+        # choose the snapshot nearest to the reference_date
+        best_offset = math.inf
+        best_timestamp = None
+
+        for timestamp_str in req.text.strip().split():
+            timestamp = dt.datetime.strptime(timestamp_str, "%Y%m%d%H%M%S")
+            offset = abs((timestamp - reference_date).total_seconds())
+
+            if offset < best_offset:
+                best_offset = offset
+                best_timestamp = timestamp_str
+
+        if best_timestamp is None:
+            req = None
+        else:
+            # crawl the webpage
+            wm_url = f'https://web.archive.org/web/{best_timestamp}id_/{url}'
+
+            try:
+                req = requests.get(wm_url)
+                req.raise_for_status()
+            except HTTPError:
+                wait_seconds *= 2
+                continue
+
+        logging.info("Saving to %s ...", out_path)
+
+        # save the whole response
+        # because HTTP headers contains more info about the snapshot
+        try:
+            with open(out_path, "xb") as fout:
+                pickle.dump(req, fout)
+        except FileExistsError:
+            continue
+
+        wait_seconds = 1
+
+
+if __name__ == "__main__":
+    main()
+
