17d7c35dee0f6dc5787f9be73cfb53d438837c59
CUI Hao <cuih7@uci.edu>
2022-07-14 16:00:04 -0700

    replace CollectionAnnotator with CollectionAnnotator2

--- privacy_policy_analyzer/scripts/run_annotators.py
+++ privacy_policy_analyzer/scripts/run_annotators.py
@@ -3,7 +3,7 @@
 import argparse
 
 import spacy
-from privacy_policy_analyzer.collection_annotator2 import CollectionAnnotator2
+from privacy_policy_analyzer.collection_annotator import CollectionAnnotator
 from privacy_policy_analyzer.coreference_annotator import CoreferenceAnnotator
 from privacy_policy_analyzer.document import PolicyDocument
 from privacy_policy_analyzer.purpose_annotator import PurposeAnnotator
@@ -22,7 +22,7 @@ def main():
     annotators = [
         SubsumptionAnnotator(nlp),
         CoreferenceAnnotator(nlp),
-        CollectionAnnotator2(nlp),
+        CollectionAnnotator(nlp),
         PurposeAnnotator(nlp),
         ListAnnotator(nlp),
     ]
