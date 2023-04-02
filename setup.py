93819704e9f74ff908f9258cefb59f74dfec51fe
CUI Hao <cuih7@uci.edu>
2023-04-01 19:11:59 -0700

    add misc files

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='Privacy Policy Analyzer',
+    author='UCI Networking Group',
+    packages=['privacy_policy_analyzer'],
+)
